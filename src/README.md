7c063efe0be79adbac7fed9cf5d22586524aab63
Z.Shang <shangzhanlin@gmail.com>
2015-12-20 21:03:09 +0000

    something

---
+++ src/README.md
@@ -0,0 +1,5 @@
+#Including
+- Multi Modes Text Editor
+- On-Screen Keyboard
+- Static Analysis Engine
+- (Maybe) Context Based Completion
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ app.on('window-all-closed', function(){
 });
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 1280, height: 800});
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
