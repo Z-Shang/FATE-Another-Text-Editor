1b61c61f320bcb5edade7a542688697250553fc4
Z.Shang <shangzhanlin@gmail.com>
2015-12-20 09:40:47 +0000

    add main.js and index.html, update package.json

---
+++ src/main.js
@@ -0,0 +1,20 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', function(){
+    app.quit();
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+    });
+});
