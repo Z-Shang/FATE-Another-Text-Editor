cbd7ec82379a326281127804d4651aaa83e4fc91
Delton Ding <dsh0416@gmail.com>
2015-12-23 02:31:15 +0800

    1. Small modifies for fixing the problems made by inaccurate DPI system by Windows. 2. Maximize the window when it starts

--- src/main.js
+++ src/main.js
@@ -19,13 +19,14 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
-    minWidth: 992,
+    minWidth: 1000,
     minHeight: 310
   });
 
   mainWindow.setMenu(null); // Diable the Native Menu on Windows, use Context Menu inside the HTML.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.show();
+  mainWindow.maximize();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
