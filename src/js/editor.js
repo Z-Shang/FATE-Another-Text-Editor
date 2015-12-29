62df8c8eb4bc4dba3dbfa16a9a459bede02daa5b
Z.Shang <shangzhanlin@gmail.com>
2015-12-29 14:17:43 +0000

    add keyboard injection

--- src/js/editor.js
+++ src/js/editor.js
@@ -5,3 +5,16 @@ feditor_proto.attachedCallback = function() {
 }
 
 var FE = document.registerElement('fate-editor', { prototype: feditor_proto } );
+
+var keyboard_proto = Object.create(HTMLElement.prototype);
+
+keyboard_proto.attachedCallback = function() {
+    var option = this.getAttribute('side');
+    if(option == 'left'){
+        this.innerHTML = "<em>Left Keyboard</em>";
+    }else if(option == 'right'){
+        this.innerHTML = "<em>Right Keyboard</em>";
+    }
+}
+
+var FK = document.registerElement('fate-keyboard', { prototype: keyboard_proto } );
