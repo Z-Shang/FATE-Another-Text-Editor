e83745019dd7a70507116ad8a7790e714637260b
Delton Ding <dsh0416@gmail.com>
2016-03-05 02:44:58 +0800

    Components the intelligent hint

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,16 +1,27 @@
 'use strict';
+// The debug flag should be deleted when publishing.
 Vue.config.debug = true
 
 var globalVar = {
-  content: '',
-  shifted: false
+  editor: {
+    content: '',
+    shifted: false
+  },
+  hint: {
+    list: [
+      ['Error', "line 5: 'scope' is not defined."],
+      ['Error', "line 6: 'let' requires for ECMAScript 6."],
+      ['Error', "line 7: 'const' requires for ECMAScript 6."],
+      ['Warning', "line 22: 'foo' is defined but not use."],
+      ['Warning', "line 27: 'bar' is defined but not use."]
+  ]}
 };
 
 Vue.component('fate-editor', {
   template: "<div v-html='render()' id='edit-area'/>",
   methods: {
     render: function() {
-      var lines =  globalVar.content.split('\n');
+      var lines =  globalVar.editor.content.split('\n');
       var html = '';
       for (var i = 0; i < lines.length; i++){
         html += '<p>' + lines[i] + '</p>';
@@ -25,26 +36,29 @@ Vue.component('fate-keyboard-key', {
   template: '<button class="keyboard-key" v-on:click="click" v-bind:style="{width:100*width+\'%\'}">{{key}}</button>',
   methods: {
     click: function() {
-      if (!globalVar.shifted) {
+      if (!globalVar.editor.shifted) {
         switch (this.key) {
           case 'Tab':
-            globalVar.content += '&nbsp&nbsp';
+            globalVar.editor.content += '&nbsp&nbsp';
             break;
           case 'Enter':
-            globalVar.content += '\n';
+            globalVar.editor.content += '\n';
             break;
           case 'Del':
-            globalVar.content = globalVar.content.slice(0, -1);
+            globalVar.editor.content = globalVar.editor.content.slice(0, -1);
             break;
           case 'Shift':
-            globalVar.shifted = true;
+            globalVar.editor.shifted = true;
+            break;
+          case 'Space':
+            globalVar.editor.content += '&nbsp';
             break;
           default:
-            globalVar.content += this.key.toLowerCase();
+            globalVar.editor.content += this.key.toLowerCase();
         }
       } else {
-        globalVar.shifted = false;
-        globalVar.content += this.key;
+        globalVar.editor.shifted = false;
+        globalVar.editor.content += this.key;
       }
     }
   }
